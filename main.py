db5de612ae4d7e984c2a0a79d6dd2d0051ce8177
= <sohailahmed34280@gmail.com>
2025-04-06 22:44:17 +0500

    Allow Cross-Origin Resource Sharing

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend URL
+    allow_origins=["http://localhost:3000", "https://fruit-classifier-smoky.vercel.app/"],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
