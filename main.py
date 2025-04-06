267a8af5c365155b90b727deecdbfc23c289ab8c
= <sohailahmed34280@gmail.com>
2025-04-06 22:00:14 +0500

    update entry point

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ async def root():
     return {"message": "Welcome to Image Classification API"}
 
 # # Run the application
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
