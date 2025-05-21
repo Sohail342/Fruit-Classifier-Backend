f48f456849f042ee53d20fe0343ecb829fa5c263
= <sohailahmed34280@gmail.com>
2025-05-21 22:01:14 +0500

    root path

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from app.api.routes import router as api_router
 
 # Create FastAPI app
 app = FastAPI(
+    root_path="/api",
     title="Image Classification API",
     description="API for image classification using TensorFlow",
     version="1.0.0"
